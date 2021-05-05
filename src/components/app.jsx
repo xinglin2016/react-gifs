3f08f6efbae180ff5a4645fe717cd5358a6ad40b
xinglin2016 <lixinglin0829@gmail.com>
2021-05-04 23:44:32 -0400

    add id for gif

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -8,7 +8,8 @@ class App extends Component {
   render () {
     const gifs = [
       { id: "QTZi3cWodE9w7LcehY" },
-      { id: "VhtuzduQ8QV6SLAYyf" }
+      { id: "VhtuzduQ8QV6SLAYyf" },
+      { id: "3o6nUSA4aoFMvfLAKQ" }
     ];
     return (
         <div>
